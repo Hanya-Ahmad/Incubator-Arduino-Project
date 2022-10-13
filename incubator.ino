bed29d7b8dc6ee6d00bca4a40d700d078bd1ee9a
Hanya Ahmad <93945902+Hanya-Ahmad@users.noreply.github.com>
2022-10-13 13:56:34 +0200

    Update incubator.ino

--- incubator.ino
+++ incubator.ino
@@ -1,49 +1,58 @@
 #include <SoftwareSerial.h>
-
-
-int relaypin = 8;
-
 //DHT imports
-#include <dht.h>
-#define dht_apin A0 // Analog Pin sensor is connected to
-int min_temp = 36.3;
-SoftwareSerial bluetooth(0, 1); // RX | TX
-int max_temp = 39;
+// #include "DHT.h"
+#define dht_dpin A0  // Analog Pin sensor is connected to
+int relayPin = 7;
+#include "dht.h"
+
 dht DHT;
+
+int min_temp = 30;
+SoftwareSerial MyBlue(0, 1);  // TX | RX
+int max_temp = 32;
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("Enter AT commands:");
- 
-
-    // HC-06 default serial speed is 9600
-  bluetooth.begin(9600);
+  // Serial.println("Enter AT commands:");
+  pinMode(relayPin, OUTPUT);
+  // HC-06 default serial speed is 9600
+  // bluetooth.begin(9600);
   Serial.begin(9600);
 }
 
 void loop() {
-    DHT.read11(dht_apin);
-    
-    Serial.print("Current humidity = ");
-    bluetooth.print(DHT.humidity);
-    Serial.print("%  ");
-    bluetooth.print("temperature = ");
-    Serial.print(DHT.temperature); 
-    Serial.println("C  ");
-
-
-      //here we run a check on the temperature.
-if (DHT.temperature <= min_temp)
-  {
-    //this means that temperature is low, we must turn on the heat source (relay)
-    digitalWrite(relaypin, HIGH);
-  }
-  else if( DHT.temperature >= max_temp)
-  {
-    //this means that the temperature is now optimal, we must turn off the heat source
-    digitalWrite(relaypin, LOW);
-  }
-  
+  //from bluetooth to Terminal.
+  if (MyBlue.available())
+    Serial.write(MyBlue.read());
+  //from termial to bluetooth
+  if (Serial.available())
+    MyBlue.write(Serial.read());
+  checkT_H();
+  printReadings();
   delay(500);
+}
+
+void printReadings() {
+  DHT.read11(dht_dpin);
+  // Serial.print("Current readHumidity() = ");
+  Serial.print(DHT.temperature);
+  // Serial.println(analogRead())
+  Serial.print(" ");
+  Serial.println(DHT.humidity);
+  // Serial.print("%  ");
+  // Serial.print("temperature = ");
+  // Serial.println("°C  ");
+}
+
+void checkT_H() {
+  //here we run a check on the temperature.
+  if (DHT.temperature <= min_temp) {
+    //this means that temperature is low, we must turn on the heat source (relay)
+    digitalWrite(relayPin, HIGH);
+  } else if (DHT.temperature >= max_temp) {
+    //this means that the temperature is now optimal, we must turn off the heat source
+    digitalWrite(relayPin, LOW);
+  } 
 }
